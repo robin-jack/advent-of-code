997298ea13b68bb74b9ce5beb17464066d6e8e2d
robin jack <robin10jack@gmail.com>
2025-12-10 01:45:19 +0100

    day5 second half

--- 2025/day5.py
+++ 2025/day5.py
@@ -4,8 +4,6 @@ def fresher():
 	fresh = 0
 	ranges = []
 	with open("inputs/day5.txt", "r") as file:
-		smol = 999999999999999999
-		big = 1
 		# get ranges
 		for line in file:
 			if line.strip() == "":
@@ -23,8 +21,55 @@ def fresher():
 	# print(fresh)
 	return fresh
 
+# --- Part Two ---
+
+def get_ranges():
+	ranges = []
+	with open("inputs/day5.txt", "r") as file:
+		# get ranges
+		for line in file:
+			if line.strip() == "":
+				break
+			ranges.append([int(n) for n in line.strip().split("-")])
+	return ranges
+
+def fresherizer(sm):
+	smallest = float('inf')
+	base = [0, 0]
+	nex = 0
+	# bingo bongo bango sequence
+	ranges = get_ranges()
+	for k in ranges:
+		if sm < k[0] < smallest:
+			smallest = k[0]
+			base = k
+
+	# me quiero pegar un tiro 3 horas pa esto
+	for j in ranges:
+		for i in ranges:
+			# print(f"{smallest} <= {i[0]} <= {base[1]} and {i[0]} >= {base[0]} and {i[1]} > {base[1]}")
+			if smallest <= i[0] <= base[1] and i[0] >= base[0] and i[1] > base[1]:
+				base = i
+	# print("####################", smallest, base[1])
+	return smallest, base[1]
+
+def freshino():
+	fresh = 0
+	sm = 0
+	for i in range(len(get_ranges())):
+		sm, bg = fresherizer(sm)
+		if sm == float('inf'):
+			break
+		fresh += ((bg+1) - sm)
+		sm = bg
+	return fresh
 
 
 if __name__ == "__main__":
 	f = fresher()
 	print(f)
+
+	print("---")
+
+	i = freshino()
+	print(i)
